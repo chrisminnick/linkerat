6b592d2ee0b6b969d0bad3b2444b61a877b22700
chrisminnick <chris@watzthis.com>
2017-08-10 08:10:35 -0700

    more routes

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ var express = require('express');
 var app = express();
 
 var port = 8000;
+var nav = [
+    {
+        link: '/',
+        text: 'Home'
+    },{
+        link: '/links',
+        text: 'All Links'
+    },{
+        link: '/users',
+        text: 'Users'
+    }];
 
-var linkRouter = require('./src/routes/linkRoutes');
+var linkRouter = require('./src/routes/linkRoutes')(nav);
 
 app.use(express.static('public'));
 app.set('views','./src/views');
--- src/routes/linkRoutes.js
+++ src/routes/linkRoutes.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var linkRouter = express.Router();
 
-var links = [
-    {
-        name: "Google",
-        url: "http://www.google.com"
-    }
-];
+var router = function(nav){
+    var links = [
+        {
+            name: "Google",
+            url: "http://www.google.com"
+        }
+    ];
 
-linkRouter.route('/')
-    .get(function(req,res){
-        res.render('pages/linkListView',{
-            title:'All the Links',
-            links:links
+    linkRouter.route('/')
+        .get(function(req,res){
+            res.render('pages/linkListView',{
+                title:'All the Links',
+                links:links,
+                nav:nav
+            });
         });
-    });
+    return linkRouter;
+};
 
 
-module.exports = linkRouter;
+module.exports = router;
