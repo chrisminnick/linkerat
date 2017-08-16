941083ecce1e758d159b0dbc3b90dad00eb415db
chrisminnick <chris@watzthis.com>
2017-08-16 16:30:33 -0700

    working on sign in

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -2,31 +2,31 @@ var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     mongodb = require('mongodb').MongoClient;
 
-module.exports = function() {
+module.exports = function () {
     passport.use(new LocalStrategy({
-        usernameField: 'userName',
-        passwordField: 'password'
-    },
-    function(username,password,done){
-        var url = 'mongodb://localhost:27017/linkerat';
-        mongodb.connect(url, function(err,db){
-            var collection = db.collection('users');
-            collection.findOne({username: username},
-            function(err, results) {
-                if(results.password === password) {
-                    var user = results;
-                    done(null, user);
-                } else {
-                    done(null, false, {message:'bad'});
-                }
+            usernameField: 'userName',
+            passwordField: 'password'
+        },
+        function (username, password, done) {
+            var url = 'mongodb://localhost:27017/linkerat';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                collection.findOne({username: username},
+                    function (err, results) {
+                        console.log(results.password);
+                        console.log(password);
 
-            })
-        });
-        var user = {
-            username: username,
-            password: password
-        };
-        done(null, user);
+                        if (results.password === password) {
+                            var user = results;
+                            done(null, user);
+                        } else {
+                            done(null, false, {message: 'bad'});
+                        }
+
+                    })
+            });
+
+
         }
     ));
 };
